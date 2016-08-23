43640ef36ba1070d01c186c72f7b62499beedb62
Gabriel Zolnierczuk <gabriel@MacBook-Pro-Gabriel.local>
2016-08-23 13:16:56 +0200

    initial pods

---
+++ Example/Pods/Target Support Files/SimpleMavenFramework/SimpleMavenFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SimpleMavenFramework : NSObject
+@end
+@implementation PodsDummy_SimpleMavenFramework
+@end
---
+++ Example/Pods/Target Support Files/SimpleMavenFramework/SimpleMavenFramework-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SimpleMavenFrameworkVersionNumber;
+FOUNDATION_EXPORT const unsigned char SimpleMavenFrameworkVersionString[];
+
